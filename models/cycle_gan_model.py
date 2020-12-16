716007e706db6fe63a876a13f51c4221ae512067
Xinyang <xylithu@gmail.com>
2020-12-16 14:22:48 +0800

    Update cycle_gan_model.py

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -41,8 +41,8 @@ class CycleGANModel(BaseModel):
         if is_train:
             parser.add_argument('--lambda_A', type=float, default=10.0, help='weight for cycle loss (A -> B -> A)')
             parser.add_argument('--lambda_B', type=float, default=10.0, help='weight for cycle loss (B -> A -> B)')
-            parser.add_argument('--threshold_A', type=float, default=92, help='weight for cycle loss (A -> B -> A)')
-            parser.add_argument('--threshold_B', type=float, default=195, help='weight for cycle loss (B -> A -> B)')
+            parser.add_argument('--threshold_A', type=float, default=70, help='weight for cycle loss (A -> B -> A)')
+            parser.add_argument('--threshold_B', type=float, default=220, help='weight for cycle loss (B -> A -> B)')
             parser.add_argument('--lambda_identity', type=float, default=0, help='use identity mapping. Setting lambda_identity other than 0 has an effect of scaling the weight of the identity mapping loss. For example, if the weight of the identity loss should be 10 times smaller than the weight of the reconstruction loss, please set lambda_identity = 0.1')
 
         return parser
